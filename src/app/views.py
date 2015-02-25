8104b98332181775898830576bbf1f5623b00d20
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-02-25 17:05:10 -0500

    EC-22 Implemented user sessions with cookies. Can successfully authenticate now

--- src/app/views.py
+++ src/app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 import logging
 
 #For Dev Purposes Only. This logger object can be identified as 'apps.view'
@@ -45,18 +47,36 @@ def register2(request):
     )
 
 def menu(request):
-    #TODO: Block access to menu with a sign-in
-    return render(
-        request,
-        'app/menu.html'
-    )
+    # TODO: More Elegantly, Block access to menu with a sign-in
+    if request.user.is_authenticated():
+        return render(
+            request,
+            'app/menu.html'
+        )
+    else:
+        return register2(request)
 
-#TODO: Remove this method and correctly handle HTTP Post requests for login/register!
-@csrf_exempt
-def nullHandler(request):
-    #This method does nothing other than log the request it receives.
-    html = "<html><body>Your request was processed</body></html>"
+# TODO: Remove this method and correctly handle HTTP Post requests for login/register!
+
+def nullhandler(request):
+    # This method does nothing other than log the request it receives.
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            html = "<html><body>Your request was processed correctly and you are logged in</body></html>"
+        else:
+            # print ("Invalid login details: {0}, {1}".format(username, password))
+            html = "<html><body>There was a problem with your request</body></html>"
     logger.debug( str(request.POST) )
     return HttpResponse(html)
 
+def logouthandler(request):
+    logout(request)
+    html = "<html><body>Your request was processed correctly and you are logged out</body></html>"
+    return(html)
 
