248b2f3d47b9983c737999b9ff542dc5c61d0d42
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-02-25 20:08:51 -0500

    EC-14 UML generated code and hand-organized subsystems

---
+++ misc/AutogeneratedCode20150225/semester/semester.py
@@ -0,0 +1,10 @@
+class Semester(object):
+	def __init__(self):
+		self.Summer1 = None
+		self.Summer2 = None
+		self.SummerFull = None
+		self.Fall = None
+		self.Winter = None
+		self.FallAndWinter = None
+
+
---
+++ src/app/subsystem/database/studentcatalog.py
@@ -0,0 +1,11 @@
+class StudentCatalog(object):
+	def enrollStudent(student):
+		pass
+
+	def removeStudent(student):
+		pass
+
+	def __init__(self):
+		pass
+
+
