4015c50e8a8ac6d4f14487f157915e548f34cbb1
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-02-25 20:08:51 -0500

    EC-14 UML generated code and hand-organized subsystems

---
+++ misc/AutogeneratedCode20150225/preferences/preferences.py
@@ -0,0 +1,7 @@
+class Preferences(object):
+	def __init__(self):
+		self.numberOfCourses = None
+		self.breakTime = None
+		self.dayOff = None
+
+
---
+++ src/app/subsystem/event/dayoftheweek.py
@@ -0,0 +1,11 @@
+class DayOfTheWeek(object):
+	def __init__(self):
+		self.Sunday = None
+		self.Monday = None
+		self.Tuesday = None
+		self.Wednesday = None
+		self.Thursday = None
+		self.Friday = None
+		self.Saturday = None
+
+
