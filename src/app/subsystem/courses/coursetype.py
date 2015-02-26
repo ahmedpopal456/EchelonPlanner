16dd0ce6531929934177f8a84e3ef64930361035
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-02-25 20:08:51 -0500

    EC-14 UML generated code and hand-organized subsystems

---
+++ src/app/subsystem/courses/coursetype.py
@@ -0,0 +1,9 @@
+class CourseType(object):
+	def __init__(self):
+		self.Core = None
+		self.TechnicalElective = None
+		self.GeneralElective = None
+		self.BasicScience = None
+		self.NonAccredited = None
+
+
