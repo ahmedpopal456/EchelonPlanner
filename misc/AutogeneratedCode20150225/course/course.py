b184768e86d44c59b5e46bd79460aca4583009c8
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-02-25 20:08:51 -0500

    EC-14 UML generated code and hand-organized subsystems

---
+++ misc/AutogeneratedCode20150225/course/course.py
@@ -0,0 +1,19 @@
+class Course(object):
+	def addStudent(IDNumber):
+		pass
+
+	def removeStudent(IDNumber):
+		pass
+
+	def __init__(self):
+		self.name = None
+		self.department = None
+		self.number = None
+		self.type = None
+		self.credits = None
+		self.prerequisites = None
+		self.equivalence = None
+		self.yearSpan = None
+		self.sections = None
+
+
