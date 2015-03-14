9381285cd59d2649c9c61065074fccc21d5e3a8c
Nicole Cappadocia-Assaly <nicolecappadocia@gmail.com>
2015-03-14 15:42:44 -0400

    EC-65 Unit testing - Empty template for the unit tests for course, database, and schedule subsystems

---
+++ test/test_schedule.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class TestSchedule(unittest.TestCase):
+    def setUp(self):
+        print("12")
+        # setup
+
+    def test_saveSchedule(self):
+        print("13.1")
+        # save schedule
+        # ensure that it is now in student's record and all course capacities have been decremented?
+
+    def test_saveSchedule_courseFull(self):
+        print("13.2")
+        # save schedule
+        # not enough capacity in a course error
+        # maybe test for in lecture, tutorial and lab separately
+
+    def test_generateSchedule_noPreferences(self):
+        print("14.1")
+        # generate schedule with no preferences selected
+
+    def test_generateSchedule_withPreferences(self):
+        print("14.2")
+        # generate schedule with some preferences
+
+    def test_generateSchedule_noPossibleSchedules(self):
+        print("14.3")
+        # try to generate a schedule with so many preferences that it is impossible
+
+if __name__ == '__main__':
+    unittest.main()
