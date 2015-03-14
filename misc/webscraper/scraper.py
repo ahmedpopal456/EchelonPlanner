95b997dbcd480cadfc47a16cb06d375577e47e02
Thinesh <th.thinesh@gmail.com>
2015-03-14 04:50:33 -0400

    EC-7 : Now scrapes all courses on ListOfCoursesToScrape.txt.
    Had to remove courses that were outdated and not offered anymore from list. Also removed a 1 credit course SOEN 491, because it was formatted wrong, should not be a problem.
    
    Currently outputs to a text file, but the format is arranged to make it easy to store in tables once I figure that out.
    
    Changed so that you just run scrapper once now, extract is a function that scrapper calls.
    
    What is missing:
     - Differentiating pre-reqs from co-reqs, but we can use this for now
     - Some pre-reqs are cegep level courses, so we'll have to add those to the DB/student record somehow, or remove them manually perhaps after.
     - auto-storing in database

--- misc/webscraper/scraper.py
+++ misc/webscraper/scraper.py
@@ -1,9 +1,35 @@
 import urllib.request
 import urllib.parse
 import urllib.error
+import re
+import extract
 
 concordiaSite = urllib.request.urlopen("http://fcms.concordia.ca/fcms/asc002_stud_all.aspx")
 #print (concordiaSite.read())
+
+#open list of courses to scrape, and put into list
+coursedept = ""
+coursenum = ""
+courselist = []
+unfilteredcourselist = open("ListOfCourseToScrape.txt", "r").readlines()
+
+#to debug courses not  offered, timeconsuming to go through whole list
+#unfilteredcourselist = open("debugcourselist.txt").readlines()
+
+debugcourselist = open("debugcourselist.txt","w")
+
+for lines in unfilteredcourselist:
+        coursedeptnum = re.search('[A-Z]{4} \d{3}', lines)
+        try:
+            coursedeptnum = coursedeptnum.group(0)
+            coursedept = coursedeptnum[:4]
+            coursenum = coursedeptnum[-3:]
+            courselist.append([coursedept, coursenum])
+        except:
+            coursedeptnum = ""
+
+for i in courselist:
+    debugcourselist.write( i[0]+ " "+ i[1]+ '\n')
 
 #Make new Request
 HardcodedParamsFiles = open("hardcodedParams.txt")
@@ -15,8 +41,6 @@ Params["ctl00$PageBody$ddlYear"]="2014"
 Params["ctl00$PageBody$ddlSess"]="A"
 Params["ctl00$PageBody$ddlLevl"]="U"
 Params["ctl00$PageBody$ddlDept"]="0403"
-Params["ctl00$PageBody$txtCournam"]="comp"
-Params["ctl00$PageBody$txtCournum"]="352"
 Params["ctl00$PageBody$txtKeyTtle"]=""
 Params["ctl00$PageBody$ddlClaWeek"]="+"
 Params["ctl00$PageBody$ddlClaDays"]="+"
@@ -25,17 +49,19 @@ Params["ctl00$PageBody$ddlCampus"]="+"
 
 #You can modify the POST param data 
 
+for course in courselist:
+    Params["ctl00$PageBody$txtCournam"] = course[0]
+    Params["ctl00$PageBody$txtCournum"] = course[1]
 
-#Send the huge string of bytes.
-data = (HardcodedParams+Params["ctl00$PageBody$ddlSess"]+\
-       "&ctl00%24PageBody%24ddlLevl="+Params["ctl00$PageBody$ddlLevl"]+\
-       "&ctl00%24PageBody%24ddlDept="+Params["ctl00$PageBody$ddlDept"]+\
-       "&ctl00%24PageBody%24txtCournam="+Params["ctl00$PageBody$txtCournam"]+\
-       "&ctl00%24PageBody%24txtCournum="+Params["ctl00$PageBody$txtCournum"]+\
-       "&ctl00%24PageBody%24txtKeyTtle=&ctl00%24PageBody%24ddlClaWeek=+&ctl00%24PageBody%24ddlClaDays=+&ctl00%24PageBody%24ddlTimeBtw=+&ctl00%24PageBody%24ddlCampus=+").encode('utf-8')
+    #Send the huge string of bytes.
+    data = (HardcodedParams+Params["ctl00$PageBody$ddlSess"]+\
+           "&ctl00%24PageBody%24ddlLevl="+Params["ctl00$PageBody$ddlLevl"]+\
+           "&ctl00%24PageBody%24ddlDept="+Params["ctl00$PageBody$ddlDept"]+\
+           "&ctl00%24PageBody%24txtCournam="+Params["ctl00$PageBody$txtCournam"]+\
+           "&ctl00%24PageBody%24txtCournum="+Params["ctl00$PageBody$txtCournum"]+\
+           "&ctl00%24PageBody%24txtKeyTtle=&ctl00%24PageBody%24ddlClaWeek=+&ctl00%24PageBody%24ddlClaDays=+&ctl00%24PageBody%24ddlTimeBtw=+&ctl00%24PageBody%24ddlCampus=+").encode('utf-8')
 
-concordiaSite = urllib.request.urlopen("http://fcms.concordia.ca/fcms/asc002_stud_all.aspx",data)
-#print(concordiaSite.read().decode('utf-8'))
-output = open("output.txt","w")
-output.write(str(concordiaSite.read().decode('utf-8')))
-output.close()
+    concordiaSite = urllib.request.urlopen("http://fcms.concordia.ca/fcms/asc002_stud_all.aspx",data)
+
+
+    extract.extract(str(concordiaSite.read().decode('utf-8')))
