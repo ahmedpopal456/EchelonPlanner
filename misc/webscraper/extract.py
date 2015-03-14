28cc71cb6409d3f4d38b38ef584fbafb8726df3b
Thinesh <th.thinesh@gmail.com>
2015-03-14 04:50:33 -0400

    EC-7 : Now scrapes all courses on ListOfCoursesToScrape.txt.
    Had to remove courses that were outdated and not offered anymore from list. Also removed a 1 credit course SOEN 491, because it was formatted wrong, should not be a problem.
    
    Currently outputs to a text file, but the format is arranged to make it easy to store in tables once I figure that out.
    
    Changed so that you just run scrapper once now, extract is a function that scrapper calls.
    
    What is missing:
     - Differentiating pre-reqs from co-reqs, but we can use this for now
     - Some pre-reqs are cegep level courses, so we'll have to add those to the DB/student record somehow, or remove them manually perhaps after.
     - auto-storing in database

--- misc/webscraper/extract.py
+++ misc/webscraper/extract.py
@@ -1,12 +1,42 @@
 from bs4 import BeautifulSoup
 import re
 
+# Master DB file
+outputDB = open("outputDB.txt", "w")
+
+
 def extractSemester(text):
+
+    department = ""
+    number = ""
+    name = ""
+    Credits = ""
+    prereq = []
+
+    for index, line in enumerate(text):
+        # Find the Course, then extract Dept, Num, Name and Credits
+        if (len(line))is 9 and re.match('[A-Z]{4} \d{3}', line):
+            department = line[:4]
+            number = line[-4:-1]
+            name = text[index+1][:-1]  #remove trailing newline from name
+            Credits = text[index+2][:1]
+
+        #Extract Preq
+        if line.find("Prerequisite") is not -1:
+            line = text[index+1]
+            try:
+                prereq = re.findall('[A-Z]{4} \d{3}',line)
+            except AttributeError:
+                prereq = ["NONE"]
+
+    print('Course: {}, {}, {}, {} Credits, {}\n'.format(department, number, name, Credits, prereq)) # goes into DB course table
+    outputDB.write('Course: {}, {}, {}, {} Credits, {}\n'.format(department, number, name, Credits, prereq))
+
     semesterlist = []
     position = []
     index = 0
     for i in text:
-        if i == "Summer\n" or i == "Fall\n" or i == "Winter\n" or i=="Fall&Winter\n":
+        if i == "Summer\n" or i == "Fall\n" or i == "Winter\n" or i=="Fall&Winter;\n":
             position.append(index)
         index+=1
 
@@ -17,18 +47,35 @@ def extractSemester(text):
     semesterlist.append(text[position[j]:])
 
     for semester in semesterlist:
-        extractLecture(semester)
+        extractLecture(semester, department, number)
 
     return semesterlist
 
 
-def extractLecture(semester):
+def extractLecture(semester, department, number):
+
+    semestername = "" #winter, fall, summer, further processing if summer
+    section = ""
+    prof =""
+    starttime = ""
+    endtime = ""
+    days = ""
+    location =""
+
     lecturelist = []
     position = []
     index = 0
 
-    for i in semester:
-        if "Lect" in i:
+    for i, line in enumerate(semester):
+        if i == 0:
+            semestername = line.strip()
+            if semestername == "Summer":
+                if "May" in semester[i+1]:
+                    semestername = "Summer1"
+                else:
+                    semestername = "Summer2"
+
+        if "Lect" in line or "OnLine" in line and "NOTE" not in line:
             position.append(index)
         index+=1
     j=0
@@ -39,41 +86,93 @@ def extractLecture(semester):
     lecturelist.append(semester[position[j]:])
 
     for lecture in lecturelist:
-        extractTutorial(lecture)
+        location = lecture[2].strip() # strip new line and spaces
+        enumerate(lecture)
+        sectionline = lecture[0].split()
+        for i in sectionline:
+            if "Lect" or "OnLine" not in i:
+                section = i
+            if "OnLine" == i:
+                location = location+"(Online)"
+        section = section.strip()
+        days = lecture[1][:7]
+        matchtime = re.findall('[0-9]{2}:[0-9]{2}-', lecture[1])
+        starttime = matchtime[0][:-1]
+        matchtime = re.findall('[0-9]{2}:[0-9]{2}[)]{1}', lecture[1])
+        endtime = matchtime[0][:-1]
+        prof = lecture[3]
+        print("Lecture : {} {}, {}, {}, {}, {}, {}, {}, {}".format
+              (department, number, semestername, section, days, starttime, endtime, location, prof))
+              # would be sent to DB Lectures, with key section, and links to courses with dept+num
+        outputDB.write("Lecture : {} {}, {}, {}, {}, {}, {}, {}, {}".format
+              (department, number, semestername, section, days, starttime, endtime, location, prof))
+        extractTutorial(lecture, section, department, number)
+    outputDB.write("\n")
 
     return lecturelist
 
 
 
-def extractTutorial(lecture):
+def extractTutorial(lecture, lecturesection, department, number):
+    section = ""
+    days = ""
+    starttime = ""
+    endtime = ""
+    location = ""
     tutorialList = []
     position = []
     index = 0
 
     for i in lecture:
-        if "Tut" in i:
+        if "Tut" in i and "NOTE" not in i:
             position.append(index)
         index+=1
 
     if position.__len__() == 0:
+        #no tutorial for this section, see if there are labs
+        extractLab(lecture, lecturesection, department, number)
         return 0
 
-    j=0
+    j = 0
 
     while j < len(position)-1:
         tutorialList.append(lecture[position[j]:position[j+1]])
-        j+=1
+        j += 1
     tutorialList.append(lecture[position[j]:])
 
     for tutorial in tutorialList:
-        extractLab(tutorial)
-
+        enumerate(tutorial)
+        sectionline = tutorial[0].split()
+        for i in sectionline:
+            if "Tut" is not i:
+                section = i
+        section = section.strip()
+        days = tutorial[1][:7]
+        matchtime = re.findall('[0-9]{2}:[0-9]{2}-', tutorial[1])
+        starttime = matchtime[0][:-1]
+        matchtime = re.findall('[0-9]{2}:[0-9]{2}[)]{1}', tutorial[1])
+        endtime = matchtime[0][:-1]
+        location = tutorial[2].strip() # strip new line and spaces
+        print("Tutorial {} {} {} : {}, {}, {}, {}, {}".format
+              (department, number, lecturesection, section, days, starttime, endtime, location))
+              # would be sent to DB Tutorials, with key section, and links to Lectures with lecturesection and dept+num
+        outputDB.write("Tutorial {} {} {} : {}, {}, {}, {}, {}\n".format
+              (department, number, lecturesection, section, days, starttime, endtime, location))
+        extractLab(tutorial, section, department, number)
+    outputDB.write("\n")
+    print("\n")
     return tutorialList
 
 
-def extractLab(tutorial):
+def extractLab(tutorial, tutorialsection, dept, num):
 
     labList = []
+    section = ""
+    days = ""
+    starttime = ""
+    endtime = ""
+    location = ""
+
     position = []
     index = 0
 
@@ -93,112 +192,50 @@ def extractLab(tutorial):
     labList.append(tutorial[position[j]:])
 
     for lab in labList:
-        extractLab(lab)
+        enumerate(lab)
+        sectionline = lab[0].split()
+        for i in sectionline:
+            if "Lab" is not i:
+                section = i
+        section = section.strip()
+        days = lab[1][:7]
+        matchtime = re.findall('[0-9]{2}:[0-9]{2}-', lab[1])
+        starttime = matchtime[0][:-1]
+        matchtime = re.findall('[0-9]{2}:[0-9]{2}[)]{1}', lab[1])
+        endtime = matchtime[0][:-1]
+        location = lab[2].strip() # strip new line and spaces
+        print("Lab {} {} {} : {}, {}, {}, {}, {}".format
+              (dept, num, tutorialsection, section, days, starttime, endtime, location))
+              # would be sent to DB Lab, with key section, and links to tutorials with tutorialsection and dept+num
+        outputDB.write("Lab {} {} {} : {}, {}, {}, {}, {}".format
+              (dept, num, tutorialsection, section, days, starttime, endtime, location))
+        outputDB.write("\n")
+    print("\n")
+
+
 
     return labList
 
 
+def extract(inputFile):
+    #used as intermediary file to extract each course
+    outputClearFile = open("outputClearFile.txt", "w")
 
-inputFile = open("output.txt")
-outputClearFile = open("outputClearFile.txt", "w")
-output2 = open("output2.txt","w")
-output3 = open("output3.txt", "w")
+    soup = BeautifulSoup(inputFile)
+    table = soup.find("table", attrs={"id": "ctl00_PageBody_tblBodyShow1"})
 
-soup = BeautifulSoup(inputFile)
-table = soup.find("table", attrs={"id": "ctl00_PageBody_tblBodyShow1"})
+    # The first tr contains the date when searched, the next few are search critera '
+    # counting on the format to remain the same (i.e i correspondong to the correct number), needs to be changed, elec has 1 extra td vs comp
+    i = 1
+    for tr in table.find_all("tr"):
+        for td in tr.find_all("td"):
+            text = (td.get_text())
+            outputClearFile.write(text + "\n")
 
-# The first tr contains the date when searched, the next few are search critera '
-# counting on the format to remain the same (i.e i correspondong to the correct number), needs to be changed, elec has 1 extra td vs comp
-i = 1
-for tr in table.find_all("tr"):
-    for td in tr.find_all("td"):
-        text = (td.get_text())
-        outputClearFile.write(text + "\n")
+    outputClearFile.close()
 
-outputClearFile.close()
-
-file_content = open("outputClearFile.txt")
-file_content = file_content.readlines()
-course_info  = extractSemester(file_content)
-for semester in course_info:
-    for lines in semester:
-        output3.write(lines)
-    output3.write("----------------------------\n")
+    file_content = open("outputClearFile.txt")
+    file_content = file_content.readlines()
+    extractSemester(file_content)
 
 
-with open("outputClearFile.txt", "r") as lines:
-    for text in lines:
-        # Find the Course, then extract Dept, Num, Name and Credits
-        if (len(text))is 9 and re.match('[A-Z]{4} \d{3}', text):
-            department = text[:4]
-            number = text[-4:-1]
-            output2.write(("Department: "+department + "\n" +"Course Number: "+number + "\n"))
-            text = next(lines,0)
-            name = text
-            output2.write(("Course Name: "+name))
-            text = next(lines,0)
-            Credits = text[:1]
-            output2.write(("Credits: "+Credits + "\n"))
-        #Extract Preq
-        if text.find("Prerequisite") is not -1:
-            text = next(lines,0)
-            try:
-                prereq = re.findall('[A-Z]{4} \d{3}',text)
-                output2.write(("Prerequisites: "+str(prereq) + "\n"))
-            except AttributeError:
-                output2.write(("Prerequisites: NONE \n"))
-        #Extract  Semester Summer class
-        if text == "Summer\n":
-            text = next(lines,0)
-            output2.write("Semester: Summer"+ "\n")
-            output2.write(str(text))
-
-            #Skip lines under Summer term until it hits line with LECT XX
-            while "Lect" not in text:
-                text = next(lines,0)
-
-            #Type Lect plus section
-            output2.write(text)
-            # Under Lect get days/time
-
-            text = next(lines,0)
-            days = text[:7]
-            matchtime = re.findall('[0-9]{2}:[0-9]{2}-',text)
-            starttime = matchtime[0][:-1]
-            matchtime = re.findall('[0-9]{2}:[0-9]{2}[)]{1}',text)
-            endtime = matchtime[0][:-1]
-            output2.write("Days: "+days+ "\n" )
-            output2.write("Start Time: "+str(starttime)+ "\n" )
-            output2.write("End Time: "+str(endtime)+ "\n" )
-
-            #Location
-            text = next(lines,0)
-            location = text
-            output2.write("Location: "+location )
-
-            #Prof
-            text = next(lines,0)
-            prof = text
-            output2.write("Prof: "+prof )
-
-            while "Tut" not in text:
-                text = next(lines,0)
-
-            #Type Tut plus section
-            output2.write(text)
-            text = next(lines,0)
-            days = text[:7]
-            matchtime = re.findall('[0-9]{2}:[0-9]{2}-',text)
-            starttime = matchtime[0][:-1]
-            matchtime = re.findall('[0-9]{2}:[0-9]{2}[)]{1}',text)
-            endtime = matchtime[0][:-1]
-            output2.write("Days: "+days+ "\n" )
-            output2.write("Start Time: "+str(starttime)+ "\n" )
-            output2.write("End Time: "+str(endtime)+ "\n" )
-
-            #Location
-            text = next(lines,0)
-            location = text
-            output2.write("Location: "+location)
-
-            #TODO seperate into different functions, i.e Extractlect, extractTut.
