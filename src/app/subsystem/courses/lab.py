e1b7584f1725fcd4867e231ce898450d63e20129
ahmedpopal456 <ahmed.popal456@hotmail.com>
2015-03-16 15:02:42 -0400

    EC-56
    1- Models were moved to subsystems
    2- Migration to the database is now done through the subsystems
    3- Setting file was changed to include 'app.subsystem.courses.course' in the
        INSTALLED_APPS definition (so that it gets recognized during migration)
    4- models.py file was removed (since its contents were moved)

--- src/app/subsystem/courses/lab.py
+++ src/app/subsystem/courses/lab.py
@@ -1,5 +1,17 @@
-class Lab(object):
-	def __init__(self):
-		self.labPrefix = None
+
+from django.db import models
+from app.subsystem.courses.course import Course
+from app.subsystem.courses.lecture import Lecture
+from app.subsystem.courses.tutorial import Tutorial
 
 
+class Lab (models.Model):
+    section = models.CharField(max_length=120, default="A", primary_key=False)
+    course = models.ForeignKey(Course, primary_key=False)
+    tutorial = models.ForeignKey(Tutorial, primary_key=False, null=True, blank=True)
+    lecture = models.ForeignKey(Lecture, primary_key=False)
+
+    class Meta:
+        unique_together = ('section', 'course', 'lecture', 'tutorial')
+        app_label = 'app'
+        managed= True
--- src/app/subsystem/courses/lecture.py
+++ src/app/subsystem/courses/lecture.py
@@ -1,7 +1,15 @@
-class Lecture(object):
-	def __init__(self):
-		self.identifier = None
-		self.session = None
-		self.isOnline = None
+
+from django.db import models
+from app.subsystem.courses.course import Course
+
+class Lecture(models.Model):
+    section = models.CharField(max_length=120, default="A", primary_key=False)
+    course = models.ForeignKey(Course, primary_key=False)
+    session = models.CharField(max_length=120, default="Fall", primary_key=False)
+    isOnline = models.BooleanField(default=False, primary_key=False)
 
 
+    class Meta:
+        unique_together = ('section', 'course')
+        app_label = 'app'
+        managed= True
