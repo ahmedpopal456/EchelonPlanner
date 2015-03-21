746852a4d80bc302355f2e721389b382ff439282
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-03-21 00:47:29 -0400

    EC-12 Large refactoring of UI code to fit conventions and test use

--- src/echelon/urls.py
+++ src/echelon/urls.py
@@ -4,22 +4,21 @@ from django.contrib import admin
 # This file tells Django to map the URL strings to a particular method to call.
 # Preferably all methods should be in the views.py file
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'echelon.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+# Examples:
+# url(r'^blog/', 'app.views.index') || So, url([regular expression], [function to call])
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'app.views.index', name='index'),
-    url(r'^login/','app.views.home',name = 'home'),
-    url(r'^register/','app.views.register',name = 'register'),
-    url(r'^menu/', 'app.views.menu', name='menu'),
-    url(r'^nullhandler/', 'app.views.nullhandler', name='nullhandler'),
+    url(r'^$', 'app.views.index'),
+    url(r'^login/', 'app.views.home'),
+    url(r'^register/', 'app.views.register'),
+    url(r'^menu/', 'app.views.menu'),
+    url(r'^nullhandler/', 'app.views.nullhandler'),
     url(r'^logout/', 'app.views.logouthandler'),
-    url(r'^loginhandler/','app.views.loginhandler'),
+    url(r'^loginhandler', 'app.views.loginhandler'),
     url(r'^work_in_progress/', 'app.views.work_in_progress'),
-    url(r'^user_profile/', 'app.views.user_profile',name = 'user_profile'),
-    url(r'^changeDetails/','app.views.changeDetails',name = 'changeDetails'),
-    url(r'^changePass/','app.views.changePass',name = 'changePass'),
-    url(r'^changeEmail/','app.views.changeEmail',name = 'changeEmail'),
+    url(r'^user_profile/', 'app.views.user_profile'),
+    url(r'^change_details/', 'app.views.change_details'),
+    url(r'^change_pass/', 'app.views.change_pass'),
+    url(r'^change_email/', 'app.views.change_email'),
 )
