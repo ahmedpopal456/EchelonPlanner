6dfcf80af7dfd8945d5a815378f2e6d29a264f44
ahmedpopal456 <ahmed.popal456@hotmail.com>
2015-03-23 19:23:00 -0400

    EC-23
     User management sub-system was completed. Tasks left for this part are to actually upload the changes to the database. This will be done later.
    
      Registration task was also started, so to enable it. Note that the forms.py file will have to be deleted down the road because fields parameters are directly sent from the front end and do not have to be provided by the backend.

--- src/app/subsystem/usermanagement/programdirector/programdirector.py
+++ src/app/subsystem/usermanagement/programdirector/programdirector.py
@@ -1,5 +1,26 @@
-class ProgramDirector():
-	def __init__(self):
-		self.department = None
+from django.contrib.auth.models import User
+from django.db import models
 
 
+class ProgramDirector(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+
+    department = models.CharField(max_length=120, null=False, blank=False, default="", primary_key=False)
+
+
+def __unicode__(self):
+    return self.user.username
+
+
+def __init__(self):
+    self.department = None
+
+
+def modifyprofile():
+    pass
