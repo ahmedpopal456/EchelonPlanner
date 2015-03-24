49aa218c91a2186d4a5746de76d45cbb35915f1a
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-03-23 20:12:25 -0400

    EC-23 Eliminating build-breaking code. Must change login model.

--- src/app/views.py
+++ src/app/views.py
@@ -5,9 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 from django.contrib.auth import authenticate, login, logout
-from src.app import UserForm, StudentForm, ProfessorForm, ProgramDirectorForm
 from django.contrib.auth.models import User
-
 from app.subsystem.courses.course import *
 import logging
 
@@ -40,8 +38,6 @@ def home(request):
 
 
 def register(request):
-
-
     # A boolean value for telling the template whether the registration was successful.
     # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
@@ -51,94 +47,26 @@ def register(request):
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
         newUser = User()
-        user_form = UserForm(data=request.POST)
-        student_form = StudentForm(data=request.POST)
+        # Dump all the info in the POST requests and save it
+        # Now we hash the password with the set_password method.
+        # Once hashed, we can update the user object.
+        newUser.set_password(newUser.password)
+        newUser.save()
 
+        # Now sort out the UserProfile instance.
+        # Since we need to set the user attribute ourselves, we set commit=False.
+        # This delays saving the model until we're ready to avoid integrity problems.
 
-        # If the two forms are valid...
-        if user_form.is_valid() and student_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
+        # Now we save the UserProfile model instance.
 
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
-
-            # Now sort out the UserProfile instance.
-            # Since we need to set the user attribute ourselves, we set commit=False.
-            # This delays saving the model until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            # Did the user provide a profile picture?
-            # If so, we need to get it from the input form and put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-
-            # Now we save the UserProfile model instance.
-            profile.save()
-
-            # Update our variable to tell the template registration was successful.
-            registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors, profile_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
+        # Update our variable to tell the template registration was successful.
+        registered = True
 
     # Render the template depending on the context.
     return render(request,
             'app/register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            )
+    # Finish Register Method
 
 
 @login_required
@@ -241,6 +169,8 @@ def schedule_view(request):
 @login_required
 def browse_all_courses(request):
     courseList = Course.objects.all()  # List of all courses
+    if request.method == 'POST':
+        print()
     return render(
         request,
         'app/browse_all_courses.html',
