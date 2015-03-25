bbcb878de71c5843cac3ab3d7a425125aceb52b1
Thineshth <th.thinesh@gmail.com>
2015-03-24 21:20:04 -0400

    EC-55:
    Added searchCourses() function which takes a partial string, and returns all courses in courses database that match this partial string.
    
    Can match department, number, name, deptnum.
    
    To test in shell:
    i.e
    >>> from app.subsystem.database.coursecatalog import CourseCatalog
    >>> list = CourseCatalog.getCourse('Software')

--- src/app/subsystem/database/coursecatalog.py
+++ src/app/subsystem/database/coursecatalog.py
@@ -1,17 +1,37 @@
+from app.subsystem.courses.course import Course
+from app.subsystem.courses.lecture import Lecture
+from app.subsystem.courses.tutorial import Tutorial
+from app.subsystem.courses.lab import Lab
+from app.subsystem.event.event import Event
+from itertools import chain
+
+
 class CourseCatalog(object):
-	def getCourses(, name, number):
-		pass
 
-	def addCourse(, name, number):
-		pass
 
-	def removeCourse(, name, number):
-		pass
+    # Returns List of Courses that contain partialName in either Name or in Deptnum
+    #or Department or Number
+     def searchCourses(partialName):
+        nospacesstring = partialName.replace(" ","")
+        c1 = Course.objects.filter(name__icontains=partialName)
+        c2 = Course.objects.filter(department__icontains=partialName)
+        c3 = Course.objects.filter(number__icontains=partialName)
+        c4 = Course.objects.filter(deptnum__icontains=nospacesstring)
 
-	def modifyCourseCapacity(newCapacity):
-		pass
+        result_list = list(chain(c1, c2, c3, c4))
 
-	def __init__(self):
-		pass
+        return result_list
+
+	# def addCourse(, name, number):
+	# 	pass
+    #
+	# def removeCourse(, name, number):
+	# 	pass
+    #
+	# def modifyCourseCapacity(newCapacity):
+	# 	pass
+    #
+	# def __init__(self):
+	# 	pass
 
 
