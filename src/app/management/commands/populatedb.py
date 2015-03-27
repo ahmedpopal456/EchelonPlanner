7b8e55e9637445d7bb398881f6568ba26dc2d1d3
Thineshth <th.thinesh@gmail.com>
2015-03-27 15:04:29 -0400

    Removed exception

--- src/app/management/commands/populatedb.py
+++ src/app/management/commands/populatedb.py
@@ -23,50 +23,51 @@ class Command(BaseCommand):
             credits = course["Credits"]
             isOnline = False
             t = None
-            try:
-                c = Course(name=name, department=department, number=number, deptnum=deptnum,credits=credits, yearSpan="14-15")
+            #try:
+            c = Course(name=name, department=department, number=number, deptnum=deptnum,credits=credits, yearSpan="14-15")
 
-                for lecture in course["Lecture"]:
-                    endtime = lecture["endtime"]
-                    starttime = lecture["starttime"]
-                    prof = lecture["prof"]
-                    semester = lecture["semester"]
-                    location = lecture["location"]
-                    days = lecture["days"]
-                    if "Online" in location:
-                        isOnline = True
-                    section = lecture["section"]
-                    e = Event(days =days, starttime = starttime, endtime=endtime, location = location,
-                              semester = semester)
-                    l = Lecture(section=section, session = semester, isOnline=isOnline, event=e)
-                    l.save()
-                    c.lecture_set.add(l)
-                    c.save()
-                    for tutorial in lecture["tutorial"]:
-                        if 'starttime' in tutorial:
-                            endtime = tutorial["endtime"]
-                            starttime = tutorial["starttime"]
-                            location = tutorial["location"]
-                            days = tutorial["days"]
-                            section = tutorial["section"]
-                            e = Event(days =days, starttime = starttime, endtime=endtime, building = location, location = location,
-                              semester = semester)
-                            e.save()
-                            t = Tutorial(section=section, event=e, course=c, lecture=l)
-                            t.save()
-                            l.tutorial_set.add(t)
-                        for lab in tutorial["lab"]:
-                            endtime = lab["endtime"]
-                            starttime = lab["starttime"]
-                            location = lab["location"]
-                            days = lab["days"]
-                            section = lab["section"]
-                            e = Event(days =days, starttime = starttime, endtime=endtime, building = location, location = location,
-                              semester = semester)
-                            e.save()
-                            lab = Lab(section=section, event = e, course=c, lecture=l)
-                            lab.save()
-                            if t is not None:
-                                t.lab_set.add(lab)
-            except django.db.DatabaseError:
-                logger.warn("Course or lecture or lab or tutorial already exists: "+deptnum)
+            for lecture in course["Lecture"]:
+                endtime = lecture["endtime"]
+                starttime = lecture["starttime"]
+                prof = lecture["prof"]
+                semester = lecture["semester"]
+                location = lecture["location"]
+                days = lecture["days"]
+                if "Online" in location:
+                    isOnline = True
+                section = lecture["section"]
+                e = Event(days =days, starttime = starttime, endtime=endtime, location = location,
+                          semester = semester)
+                e.save()
+                l = Lecture(section=section, session = semester, isOnline=isOnline, event=e)
+                l.save()
+                c.lecture_set.add(l)
+                c.save()
+                for tutorial in lecture["tutorial"]:
+                    if 'starttime' in tutorial:
+                        endtime = tutorial["endtime"]
+                        starttime = tutorial["starttime"]
+                        location = tutorial["location"]
+                        days = tutorial["days"]
+                        section = tutorial["section"]
+                        e = Event(days =days, starttime = starttime, endtime=endtime, building = location, location = location,
+                          semester = semester)
+                        e.save()
+                        t = Tutorial(section=section, event=e, course=c, lecture=l)
+                        t.save()
+                        l.tutorial_set.add(t)
+                    for lab in tutorial["lab"]:
+                        endtime = lab["endtime"]
+                        starttime = lab["starttime"]
+                        location = lab["location"]
+                        days = lab["days"]
+                        section = lab["section"]
+                        e = Event(days =days, starttime = starttime, endtime=endtime, building = location, location = location,
+                          semester = semester)
+                        e.save()
+                        lab = Lab(section=section, event = e, course=c, lecture=l)
+                        lab.save()
+                        if t is not None:
+                            t.lab_set.add(lab)
+            #except django.db.DatabaseError:
+             #   logger.warn("Course or lecture or lab or tutorial already exists: "+deptnum)
