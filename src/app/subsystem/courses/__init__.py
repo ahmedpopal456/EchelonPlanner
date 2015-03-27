636053a4f695b7847226a522953b4b38d5d91464
Nauman <mirzaman94@gmail.com>
2015-03-27 17:29:49 -0400

    EC-56: course management system
    option.py, academicprogram.py, course.py worked on (implementation of enums and fields)
    
    Nauman: 2 hours

--- src/app/subsystem/courses/__init__.py
+++ src/app/subsystem/courses/__init__.py
@@ -1,6 +1,8 @@
+from app.subsystem.courses.academicprogram import AcademicProgram
+from app.subsystem.courses.option import Option
 from app.subsystem.courses.course import Course
 from app.subsystem.courses.lecture import Lecture
 from app.subsystem.courses.tutorial import Tutorial
 from app.subsystem.courses.lab import Lab
 
-__all__ = ['Course', 'Lecture', 'Tutorial', 'Lab']
+__all__ = ['AcademicProgram', 'Option', 'Course', 'Lecture', 'Tutorial', 'Lab']
--- src/app/subsystem/courses/academicprogram.py
+++ src/app/subsystem/courses/academicprogram.py
@@ -1,6 +1,18 @@
-class AcademicProgram(object):
-	def __init__(self):
-		self.name = None
-		self.credits = None
+from django.db import models
+from app.subsystem.courses.option import Option
+
+class AcademicProgram(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=120, null=False, blank=False, default="SOEN", primary_key=False)
+    credits = models.FloatField(default=0, primary_key=False)
+    option = models.ManyToManyField(Option, null=True, through='Option')
+
+    class Meta:
+        app_label = 'app'
+        managed= True
+
 
 
