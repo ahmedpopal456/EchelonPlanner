b0aba03c9c51639d0ad82e2744ad5bc7fa07cb55
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2015-03-28 02:43:19 -0400

    EC-56 Tidying up the Subsystem. Last non-aesthetic pass

--- src/app/subsystem/courses/coursetype.py
+++
@@ -1,9 +0,0 @@
-class CourseType(object):
-	def __init__(self):
-		self.Core = None
-		self.TechnicalElective = None
-		self.GeneralElective = None
-		self.BasicScience = None
-		self.NonAccredited = None
-
-
