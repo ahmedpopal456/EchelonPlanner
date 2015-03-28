4633ddf048821ee71c7ad949b0f3b3b8835374ee
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2015-03-28 00:52:49 -0400

    EC-23 User Registration now Functional. Database now reflects these changes.

--- src/app/subsystem/usermanagement/student/student.py
+++ src/app/subsystem/usermanagement/student/student.py
@@ -3,22 +3,19 @@ from django.db import models
 
 
 class Student(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
+    # This line is required. Links to a User model instance.
     user = models.OneToOneField(User)
 
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    # Override the __unicode__() method to return out something meaningful!
-
+    # Additional Stuff
     homephone = models.IntegerField(null=False, blank=False, default=0, primary_key=False)
     cellphone = models.IntegerField(null=False, blank=False, default=0, primary_key=False)
     address = models.CharField(max_length=120, null=False, blank=False, default="", primary_key=False)
+    IDNumber = models.IntegerField(null=False, blank=False, default=0, primary_key=False)
+    #somer = models.CharField(max_length=120, null=True, blank=True, default="")
 
-def __unicode__(self):
-    return self.user.username
+    def __unicode__(self):
+        return self.user.username
 
-
-def __init__(self):
-    self.__IDNumber = None
-    self.user = User()
+    class Meta:
+        app_label = 'app'
+        managed= True
