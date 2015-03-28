d31d281cfb6658f658ca3e9b0f2661b3fc23dd2c
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-03-28 16:32:33 -0400

    EC-80 Hardcoded email as username for registered users. Made appropriate changes elsewhere

--- src/app/subsystem/usermanagement/student/student.py
+++ src/app/subsystem/usermanagement/student/student.py
@@ -7,14 +7,13 @@ class Student(models.Model):
     # This line is required. Links to a User model instance.
     user = models.OneToOneField(User)
     # Student's unique academic record
-    academicRecord = models.OneToOneField(StudentRecord,null=True)
+    academicRecord = models.OneToOneField(StudentRecord, null=True)
 
     # Additional Stuff, mostly fluff
     homephone = models.IntegerField(null=False, blank=False, default=0, primary_key=False)
     cellphone = models.IntegerField(null=False, blank=False, default=0, primary_key=False)
     address = models.CharField(max_length=120, null=False, blank=False, default="", primary_key=False)
     IDNumber = models.IntegerField(null=False, blank=False, default=0, primary_key=False)
-    #somer = models.CharField(max_length=120, null=True, blank=True, default="")
 
     def __unicode__(self):
         return self.user.username
