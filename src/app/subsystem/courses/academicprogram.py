93141b882fab8a96a1c44cae65acfa0c25519c09
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2015-03-28 02:43:19 -0400

    EC-56 Tidying up the Subsystem. Last non-aesthetic pass

--- src/app/subsystem/courses/academicprogram.py
+++ src/app/subsystem/courses/academicprogram.py
@@ -3,13 +3,12 @@ from app.subsystem.courses.course import Course
 
 class AcademicProgram(models.Model):
 
-
-    def __str__(self):
-        return self.name
-
     name = models.CharField(max_length=120, null=False, blank=False, default="SOEN", primary_key=False)
     credits = models.FloatField(default=0, primary_key=False)
     course = models.ManyToManyField(Course, null=True, through='Option')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         app_label = 'app'
