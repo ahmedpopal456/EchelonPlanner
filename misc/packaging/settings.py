74ac84959db0ad32f2eb6add25776c22bf967a5a
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-03-30 22:03:13 -0400

    EC-77 Updated settings.py to reflect recent test changes with Apache

--- misc/packaging/settings.py
+++ misc/packaging/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '39*l!u5$be6#f_*nzh@+6(u40ut=#867z@r&m195dcum^_0d^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'app',
-    # 'app.subsystem'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,27 +56,17 @@ ROOT_URLCONF = 'echelon.urls'
 WSGI_APPLICATION = 'echelon.wsgi.application'
 
 # Default Login URL
-LOGIN_URL='/login/'
-
+LOGIN_URL='/login_handler/'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-# Note on Databases:
-# If you do have a local database, feel free to use it. But for convenience, we will be switching to a centralized one
-#        'USER': 'eve',
-#         'PASSWORD': 'SOEN341echelon!',
-#         'HOST': 'bbbtimmy.noip.me', # OR centcom.noip.me
-#         'PORT': '3306',
-# Note that the remote DB at centcom.noip.me is 20x slower than having a local DB on your machine
-
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
         'NAME': 'echelon',  # MySQL must contain this DB.
         'USER': 'eve',
         'PASSWORD': 'SOEN341echelon!',
-        'HOST': 'bbbtimmy.noip.me',
+        'HOST': 'localhost',
         'PORT': '3306',
         'TEST': {
             'NAME': 'test_echelon',
@@ -89,26 +78,6 @@ DATABASES = {
             }
     }
 }
-
-# Used for local testing (Put your own user and password
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'mysql.connector.django',
-#         'NAME': 'echelon',  # MySQL must contain this DB.
-#         'USER': 'root',
-#         'PASSWORD': 'SOEN341echelon!',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'TEST': {
-#             'NAME': 'test_echelon',
-#             'CREATE_DB': 'False',
-#             'CREATE_USER': 'False',
-#             'USER': '????',
-#             'PASSWORD': '????',
-#         }
-#     }
-# }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
