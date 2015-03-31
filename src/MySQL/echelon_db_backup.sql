9022a1e5bab5e422d19e5a91d9c3c770865fbab1
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-03-30 21:53:47 -0400

    EC-77 Changes to enhance packing/unpacking and making the application self-contained.

--- src/MySQL/echelon_db_backup.sql
+++ src/MySQL/echelon_db_backup.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP DATABASE IF EXISTS `echelon`;
+CREATE DATABASE `echelon`;
+USE `echelon`;
+
 --
 -- Table structure for table `app_academicprogram`
 --
