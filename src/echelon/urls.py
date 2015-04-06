7e7430453dfa1c4cf666d3875942975879d0c4bb
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-04-05 22:08:22 -0400

    EC-13 Organizing the constantly growing URL List

--- src/echelon/urls.py
+++ src/echelon/urls.py
@@ -9,29 +9,39 @@ from django.contrib import admin
 # url(r'^blog/', 'app.views.index') || So, url([regular expression], [function to call])
 
 urlpatterns = patterns('',
+    # SUDO
     url(r'^admin/', include(admin.site.urls)),
+    # DEV ONLY URLS!!!!!!
+    url(r'^nullhandler/', 'app.views.nullhandler'),
+    url(r'^work_in_progress/', 'app.views.work_in_progress'),
+
+    # User Management and general navigation
     url(r'^$', 'app.views.index'),
     url(r'^register/', 'app.views.register'),
     url(r'^menu/', 'app.views.menu'),
-    url(r'^nullhandler/', 'app.views.nullhandler'),
     url(r'^logout/', 'app.views.logouthandler'),
     url(r'^login_handler', 'app.views.login_handler'),
-    url(r'^work_in_progress/', 'app.views.work_in_progress'),
     url(r'^user_profile/', 'app.views.user_profile'),
     url(r'^change_details/', 'app.views.change_details'),
     url(r'^change_pass/', 'app.views.change_pass'),
     url(r'^change_email/', 'app.views.change_email'),
+
+    # Schedule URLs
     url(r'^schedule_make/', 'app.views.schedule_make'),
     url(r'^schedule_select/', 'app.views.schedule_select'),
     url(r'^schedule_view/', 'app.views.schedule_view'),
-    url(r'^browse_all_courses/', 'app.views.browse_all_courses'),
-    url(r'^course_create/', 'app.views.course_create'),
     url(r'^glorious_schedule_assembly/', 'app.views.glorious_schedule_assembly'),
-    url(r'^concordia_resources/', 'app.views.concordia_resources'),
     url(r'^schedule_generator/', 'app.views.schedule_generator'),
     url(r'^sched_gen_1/', 'app.views.sched_gen_1'),
+
+    # Course URLs
+    url(r'^browse_all_courses/', 'app.views.browse_all_courses'),
+    url(r'^course_create/', 'app.views.course_create'),
+
+    # Misc
+    url(r'^concordia_resources/', 'app.views.concordia_resources'),
     url(r'^favicon.ico$', RedirectView.as_view(url='/static/favicon.ico')),
-    ####
-    # API STUFF
+
+    # REST API URLs
     url(r'^course/(?P<deptnum>[A-z]{4}[0-9]{3})','app.views.course_dispatcher')
 )
