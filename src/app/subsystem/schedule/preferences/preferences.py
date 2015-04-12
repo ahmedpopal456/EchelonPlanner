bd68dc7295f02176f29dfc7f5b2f28925b1c6af3
Nicole Cappadocia-Assaly <nicolecappadocia@gmail.com>
2015-04-12 10:17:43 -0400

    EC-115 Preferences selection

--- src/app/subsystem/schedule/preferences/preferences.py
+++ src/app/subsystem/schedule/preferences/preferences.py
@@ -1,7 +1,31 @@
 class Preferences(object):
-	def __init__(self):
-		self.numberOfCourses = None
-		self.breakTime = None
-		self.dayOff = None
+
+    def __init__(self):
+        # string of days that the user wants off. ex.: "----FSD" wants a long weeked
+        self.daysOff = None
+        # list of time slots that the user wants to have.
+        # options are:
+        #   "morning" before 12
+        #   "afternoon" between 12 and 5
+        #   "evening" after 5
+        # ex.: ["morning", "evening"]
+        # the user only wants courses before 12 and after 5
+        self.timeOfDay = None
+        # list of locations that the user wants
+        # can be "SGW", "LOY", "Online"
+        self.location = None
 
 
+    """
+    method that creates a set of preferences
+    """
+    @staticmethod
+    def createPreferences(daysOff = "MTWJFSD", timeOfDay = [], location = []):
+        myPrefs = Preferences()
+
+        #TODO: make sure that what is received changes into the proper format as defined above!
+        myPrefs.daysOff = daysOff
+        myPrefs.timeOfDay = timeOfDay
+        myPrefs.location = location
+
+        return myPrefs
