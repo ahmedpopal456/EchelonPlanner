21b06a3a5899f454db781b587496236a33c7445b
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-04-20 23:29:57 -0400

    EC-13 Patches to some feature breaking bugs. A lot more need to be patched up!

--- src/echelon/settings.py
+++ src/echelon/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
         'NAME': 'echelon',  # MySQL must contain this DB.
-        'USER': 'eve',
+        'USER': 'root',
         'PASSWORD': 'SOEN341echelon!',
-        'HOST': 'bbbtimmy.noip.me',
+        'HOST': 'localhost',
         'PORT': '3306',
         # 'TEST': {
         #     'NAME': 'test_echelon',
